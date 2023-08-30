a13820ed4b1118c0c5b38987bbf5058ed575b743
Ramadevi116 <89075054+Ramadevi116@users.noreply.github.com>
2023-08-30 14:48:28 +0530

    Files/FileWriter directory

--- Files/FileWriter/Write.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-class Write{
-	public static void main(String[] args) throws Exception{
-		FileWriter f=new FileWriter("TextDoc.txt");
-		f.write((int)'R');
-		f.close();
-    }
-}
